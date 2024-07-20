e8d8142537d21b259e23e0512e5918d4ab7213b2
PranayBhatnagar <bhatnagarpranay@outlook.com>
2024-07-20 13:27:58 +0530

    tried again

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/submit', methods=['POST'])
 def submit():
