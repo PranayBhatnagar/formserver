7cdb382382ddb3f3d480958888c9de9ae1ecdf32
PranayBhatnagar <bhatnagarpranay@outlook.com>
2024-07-20 14:23:51 +0530

    form log save

--- app.py
+++ app.py
@@ -25,3 +25,6 @@ def submit():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+import logging
+logging.basicConfig(level=logging.INFO)
